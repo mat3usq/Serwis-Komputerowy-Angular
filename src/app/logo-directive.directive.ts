70614e6ebc506a51727a45c7556870d22e24f163
mat3usq <mmogielnicki123@gmail.com>
2023-12-11 22:31:10 +0100

    front i lekkie poprawki z backendu

--- src/app/logo-directive.directive.ts
+++ src/app/logo-directive.directive.ts
@@ -9,35 +9,38 @@ export class LogoDirectiveDirective {
   private originalFontSize: string;
   private originalFontWeight: string;
   private originalFontFamily: string;
+  private originalLetterSpacing: string;
   private isMouseOver: boolean = false;
 
   constructor(private el: ElementRef, private renderer: Renderer2) {
-    this.originalColor = el.nativeElement.style.color || 'black';
-    this.originalFontSize = el.nativeElement.style.fontSize || 'inherit';
+    this.originalColor = el.nativeElement.style.color || 'aliceblue';
+    this.originalFontSize = el.nativeElement.style.fontSize || '22px';
     this.originalFontWeight = el.nativeElement.style.fontWeight || 'normal';
+    this.originalLetterSpacing = el.nativeElement.style.letterSpacing || '2px';
     this.originalFontFamily = el.nativeElement.style.fontFamily || 'inherit';
 
-    this.renderer.setStyle(el.nativeElement, 'transition', 'color 0.3s, font-size 0.3s, font-weight 0.3s, font-family 0.3s');
+    this.renderer.setStyle(el.nativeElement, 'transition', 'color 0.3s, font-size 0.3s, font-weight 0.3s, font-family 0.3s, letter-spacing 0.3s');
   }
 
   @HostListener('mouseenter') onMouseEnter() {
     if (!this.isMouseOver) {
       this.isMouseOver = true;
-      this.changeStyles('red', '22px', 'bold', 'sans-serif');
+      this.changeStyles('aquamarine', '26px', 'bold', 'Poppins', '4px');
     }
   }
 
   @HostListener('mouseleave') onMouseLeave() {
     if (this.isMouseOver) {
       this.isMouseOver = false;
-      this.changeStyles(this.originalColor, this.originalFontSize, this.originalFontWeight, this.originalFontFamily);
+      this.changeStyles(this.originalColor, this.originalFontSize, this.originalFontWeight, this.originalFontFamily, this.originalLetterSpacing);
     }
   }
 
-  private changeStyles(color: string, fontSize: string, fontWeight: string, fontFamily: string): void {
+  private changeStyles(color: string, fontSize: string, fontWeight: string, fontFamily: string, letterSpacing: string): void {
     this.renderer.setStyle(this.el.nativeElement, 'color', color);
     this.renderer.setStyle(this.el.nativeElement, 'font-size', fontSize);
     this.renderer.setStyle(this.el.nativeElement, 'font-weight', fontWeight);
     this.renderer.setStyle(this.el.nativeElement, 'font-family', fontFamily);
+    this.renderer.setStyle(this.el.nativeElement, 'letter-spacing', letterSpacing);
   }
 }
--- src/app/show-report-form/show-report-form.component.ts
+++ src/app/show-report-form/show-report-form.component.ts
@@ -1,58 +1,72 @@
 import { Component } from '@angular/core';
-import { Report } from "../../models/Report";
+import { Report } from '../../models/Report';
 import { Priority } from 'src/models/Priority';
 import { Status } from 'src/models/Status';
 import { ReportsService } from 'src/services/reports.service';
 import { UserService } from 'src/services/user.service';
 import { Router } from '@angular/router';
-import { NgForm,FormBuilder, Validators, ValidationErrors, FormGroup } from '@angular/forms';
+import {
+  FormBuilder,
+  Validators,
+  ValidationErrors,
+  FormGroup,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-show-report-form',
   templateUrl: './show-report-form.component.html',
-  styleUrls: ['./show-report-form.component.css']
+  styleUrls: ['./show-report-form.component.css'],
 })
 export class ShowReportFormComponent {
   problemDescription: string = '';
   selectedPriority: Priority = Priority.normal;
   public newReport: FormGroup = new FormGroup({});
 
-  constructor(private formBuilder: FormBuilder, private reportsService: ReportsService, private userService: UserService, private router: Router) { }
+  constructor(
+    private formBuilder: FormBuilder,
+    private reportsService: ReportsService,
+    private userService: UserService,
+    private router: Router
+  ) {}
 
-  priorityOptions: Priority[] = [
-    Priority.high,
-    Priority.normal,
-  ];
+  priorityOptions: Priority[] = [Priority.high, Priority.normal];
   ngOnInit() {
     this.initForm();
   }
 
   initForm() {
     this.newReport = this.formBuilder.group({
-      problemDescription: ['', [Validators.minLength(50), Validators.required]],
-      priority: ['', Validators.required],
-      });
+      problemDescription: [[Validators.minLength(50), Validators.required]],
+      priority: [Validators.required],
+    });
   }
 
   onSubmit() {
     console.log('Opis problemu:', this.problemDescription);
     console.log('Priorytet:', this.selectedPriority);
-    const newReport = new Report(this.problemDescription, this.selectedPriority, Status.new, new Date(), this.userService.getLoggedUserId());
+    const newReport = new Report(
+      this.problemDescription,
+      this.selectedPriority,
+      Status.new,
+      new Date(),
+      this.userService.getLoggedUserId()
+    );
     console.log(newReport);
     this.reportsService.addReport(newReport);
     this.router.navigate(['/reports']);
     console.log(this.userService.getLoggedUserId());
-
   }
+
   getFormValidationErrors() {
     Object.keys(this.newReport.controls).forEach((key) => {
       const controlErrors: ValidationErrors = this.newReport.get(key)!.errors!;
-      Object.keys(controlErrors || {}).forEach(keyError => {
-        console.log(`Key control: ${key}, keyError: ${keyError}, errValue: ${controlErrors[keyError]}`);
+      Object.keys(controlErrors || {}).forEach((keyError) => {
+        console.log(
+          `Key control: ${key}, keyError: ${keyError}, errValue: ${controlErrors[keyError]}`
+        );
       });
     });
   }
-
 
   addNewReport(newReport: Report) {
     this.reportsService.addReport(newReport);
